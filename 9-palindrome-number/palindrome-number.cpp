4a390ec71565832294bcb3fcc6bc88981bcd1f05
Kanishka Vats <kanishkvats2020@gmail.com>
2026-05-23 23:56:29 +0530

    Time: 2 ms (50.93%) | Memory: 8.5 MB (65.21%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0) return false;
+        int num = x;
+        long long rev = 0;
+        while(x != 0){
+            int rem = x % 10;
+            rev = rev * 10 + rem;
+            x = x/10;
+        }
+        if(rev == num){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
