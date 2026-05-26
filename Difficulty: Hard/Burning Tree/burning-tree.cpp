c0b8c0f395012f8f4f84f67702bb6b47fafd2067
Kanishka Vats <kanishkvats2020@gmail.com>
2026-05-26 18:30:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Burning Tree/burning-tree.cpp
@@ -0,0 +1,99 @@
+/*
+class Node {
+  public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+  //create Mapping and return target Node
+    Node* createParentMapping(Node* root ,int target ,map<Node*,Node*> &nodeToParent){
+        Node* res = NULL;
+        
+        queue<Node*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            Node* front = q.front();
+            q.pop();
+            
+            if(front-> data == target){
+                res = front;
+            }
+            if(front -> left){
+                nodeToParent[front->left] = front;
+                q.push(front->left);
+            }
+            if(front->right){
+                nodeToParent[front->right] = front;
+                q.push(front->right);
+            }
+        }
+        return res;
+    }
+    int burnTree(Node* root, map<Node*, Node*> &nodeToParent){
+        map<Node*,bool> visited;
+        queue<Node*>q;
+        
+        q.push(root);
+        visited[root] = true;
+        
+        int ans = 0;
+        
+        while(!q.empty()){
+            
+            bool flag = 0;
+            int size = q.size();
+            
+            for(int i=0;i<size;i++){
+                Node* front = q.front();
+                q.pop();
+                
+                if(front -> left && !visited[front->left]){
+                    flag = 1;
+                    q.push(front->left);
+                    visited[front->left] = 1;
+                }
+                
+                if(front -> right && !visited[front->right]){
+                    flag = 1;
+                    q.push(front->right);
+                    visited[front->right] = 1;
+                }
+                
+                if(nodeToParent[front] && !visited[nodeToParent[front]]){
+                    flag = 1;
+                    q.push(nodeToParent[front]);
+                    visited[nodeToParent[front]] = 1;
+                }
+            }
+            if(flag == 1){
+                ans++;
+            }
+        }
+            return ans;
+    }
+    int minTime(Node* root, int target) {
+        // code here
+        //algo;
+        //step1: cretae nodeToparent mapping 
+        //step 2: find target node
+        //step 3: burn  the tree in the min time
+        
+  
+        map<Node* , Node*> nodeToParent;
+        Node* targetNode = createParentMapping(root,target,nodeToParent);
+        
+        int ans = burnTree(targetNode, nodeToParent);
+        
+        return ans;
+    }
+};
