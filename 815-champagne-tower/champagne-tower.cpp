9a2c4bc1bab5ac6ee56820fdfe1e99a41d06b41e
Kanishka Vats <kanishkvats2020@gmail.com>
2026-07-02 23:36:50 +0530

    Time: 5 ms (64.63%) | Memory: 17.4 MB (62.00%) - LeetSync

---
+++ 815-champagne-tower/champagne-tower.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<vector<double>> dp(query_row + 2, vector<double>(query_row + 2, 0.0));
+
+        dp[0][0] = poured;
+
+        for (int i = 0; i <= query_row; i++) {
+            for (int j = 0; j <= i; j++) {
+                double overflow = max(0.0, (dp[i][j] - 1.0) / 2.0);
+
+                if (overflow > 0) {
+                    dp[i + 1][j] += overflow;
+                    dp[i + 1][j + 1] += overflow;
+                }
+            }
+        }
+
+        return min(1.0, dp[query_row][query_glass]);
+    }
+};
