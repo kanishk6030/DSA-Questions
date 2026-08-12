9b87d53096d3188bd1b9bf788b1a38429e83b629
Kanishka Vats <kanishkvats2020@gmail.com>
2026-08-12 23:13:37 +0530

    Time: 4 ms (10.51%) | Memory: 71.6 MB (81.95%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -5,7 +5,9 @@ public:
        long long maxSum = LONG_MIN;
        for(int i = 0; i < nums.size(); i ++){
             sum = sum + nums[i];
-            maxSum = max(sum,maxSum);
+            if(sum > maxSum){
+                maxSum = sum;
+            }
             if(sum < 0) sum = 0;
        } 
        return maxSum;
