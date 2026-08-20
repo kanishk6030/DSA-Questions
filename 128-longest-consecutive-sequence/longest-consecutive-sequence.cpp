e9624bb5d53cd725c3dfbd49aa0c27caaaf60279
Kanishka Vats <kanishkvats2020@gmail.com>
2026-08-20 23:48:08 +0530

    Time: 7 ms (99.04%) | Memory: 60.2 MB (93.04%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -26,7 +26,7 @@ public:
         // Use sorting algo tc - O(n log n)
         sort(nums.begin(),nums.end());
         int count = 1;
-        int result = 1 ;
+        int result =1 ;
         for(int i= 1 ; i < nums.size();i++){
             if(nums[i] == nums[i - 1]){
                 continue;
